b32bc60b97758bdfab0627849d2bb0106bea4a7b
yenrosagala <110947560+yenrosagala@users.noreply.github.com>
2026-07-28 08:50:40 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,37 +1,20 @@
 import streamlit as st
-from modules.admin_page import show_admin_page
+from modules.admin_page import show_series_admin_page
 from modules.dashboard_page import show_dashboard_page
 from modules.report_page import show_report_page
-# 1. Tambahkan init_narrative_table pada import
-from modules.database import init_db, init_narrative_table 
-import sys
-from pathlib import Path
-from google import genai
-
-# Add project root to Python path
-ROOT_DIR = Path(__file__).resolve().parent
-if str(ROOT_DIR) not in sys.path:
-    sys.path.append(str(ROOT_DIR))
 
 st.set_page_config(page_title="Dashboard Transportasi Papua", layout="wide")
 
 def main():
-    # 2. Panggil kedua fungsi inisialisasi di sini
-    try:
-        init_db()
-        init_narrative_table() # <-- Tambahkan baris ini
-    except Exception as e:
-        st.error(f"Gagal menginisialisasi database: {e}")
-        
     st.sidebar.title("📌 Navigasi")
-    page = st.sidebar.radio("Pilih Halaman:", ["Dashboard Visualisasi", "Laporan Komparatif", "Admin & Upload Data"])
+    page = st.sidebar.radio("Pilih Halaman:", ["Dashboard Visualisasi", "Laporan Perkembangan", "Analisis Series & Admin"])
 
     if page == "Dashboard Visualisasi":
         show_dashboard_page()
-    elif page == "Laporan Komparatif":
+    elif page == "Laporan Perkembangan":
         show_report_page()
-    elif page == "Admin & Upload Data":
-        show_admin_page()
+    elif page == "Analisis Series & Admin":
+        show_series_admin_page()
 
 if __name__ == '__main__':
     main()
